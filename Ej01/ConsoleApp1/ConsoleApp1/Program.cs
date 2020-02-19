d2bde2d3ce640f43d774bb8c66102d342f61b3e9
Sergio BG <sergibelgo1@gmail.com>
2020-02-19 12:06:53 +0100

    Ejercicios proyecto principal
    Archivos principales de los distintos ejercicios

---
+++ Ej01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Ejercio 1
+            ////numero 1
+            //float x;
+            ////numero 2
+            //float y;
+            ////Definicion de numero 1
+            //Console.WriteLine("Escribe un numero por favor");
+            //x = float.Parse(Console.ReadLine());
+            ////Definicion de numero 2
+            //Console.WriteLine("Escribe el mismo numero por favor");
+            //y = float.Parse(Console.ReadLine());
+            ////Comprobamos si son el mismo numero
+            //if (x == y) 
+            //{
+            //    Console.WriteLine("Los numeros: " + x + " y " + y + " son iguales");
+            //    Thread.Sleep(2000);
+            //}
+            ////Comprobamos si el numero a es mayor que b
+            //else if (x>y)
+            //{
+            //    Console.WriteLine("El numero "+ x + " es mayor que " + y);
+            //    Thread.Sleep(2000);
+            //}
+            //// Como solo queda una opcion no hace falta especificar que sea menor que
+            //else
+            //{
+            //    Console.WriteLine("El numero " + y + " es mayor que " + x);
+            //    Thread.Sleep(2000);
+            //}
+            //Ejercicio 2
+            ////Definimos numero a pedir
+            //float x;
+            //float y;
+            ////Pedimos al usuario que lo introduzca
+            //Console.WriteLine("Escribe un numero por favor");
+            //x = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Escribe otro numero");
+            //y = float.Parse(Console.ReadLine());
+            ////Comprobamos si es par o inpar
+            //if ((x % y)==0)
+            //{
+            //    Console.WriteLine("Es multiplo");
+            //    Thread.Sleep(2000);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No es multiplo");
+            //    Thread.Sleep(2000);
+            //}
+            //Ejerciocio 3
+            //float x;
+            //Console.WriteLine("Escribe un numero por favor");
+            //x = float.Parse(Console.ReadLine());
+            //if (x<0)
+            //{
+            //    Console.WriteLine("Es negativo");
+            //    Thread.Sleep(2000);
+            //}
+            //else if (x>0)
+            //{
+            //    Console.WriteLine("Es positivo");
+            //    Thread.Sleep(2000);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Es cero");
+            //    Thread.Sleep(2000);
+            //}
+            //ejercicio 4
+            //int x;
+            //Console.WriteLine("Hola buenas ¿Que deseas?");
+            //Console.WriteLine("1. Refrán");
+            //Console.WriteLine("2. Chiste");
+            //Console.WriteLine("3. Insulto");
+            //x = int.Parse(Console.ReadLine());
+            //switch (x)
+            //{
+            //    default:
+            //        Console.WriteLine("Te has equivocado, inutil");
+            //        Thread.Sleep(2000);
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("Mas sabe el diablo por viejo que por diablo");
+            //        Thread.Sleep(2000);
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Van dos y se cae el de la derecha, y dice el del medio -Por fin no me caigo yo");
+            //        Thread.Sleep(2000);
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("MIRA, UN MONO DE TRES CABEZAS");
+            //        Thread.Sleep(2000);
+            //        break;
+            //}
+            //Ejercicio 5
+            //float x;
+            //float y;
+            //float z;
+            //Console.WriteLine("Dime tu nota primera");
+            //x = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Dime tu nota segunda");
+            //y = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Dime tu nota tercera");
+            //z = float.Parse(Console.ReadLine());
+            //Console.WriteLine("La media es " + ((x + y + z) / 3f));
+            //Thread.Sleep(2000);
+            //Ejercicio 6
+            float x;
+            Console.WriteLine("Dime tu estatura en cm");
+            x = float.Parse(Console.ReadLine());
+            if (x >= 40)
+            {
+                if (x >= 40 && x <= 140)
+                {
+                    Console.WriteLine("JAJA, BAJITO");
+                    Thread.Sleep(2000);
+                }
+                else if (x > 140 && x < 200)
+                {
+                    Console.WriteLine("Not bad");
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Console.WriteLine("DAAMMM BOY");
+                    Thread.Sleep(2000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ja ja, muy gracioso");
+                Thread.Sleep(2000);
+            }
+            
+            {
+
+            }
+        }
+    }
+}
---
+++ Prueba scrip/Prueba scrip/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Prueba_scrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prueba 01
+
+            ////Los comentarios son con doble barra
+            //// int numeros enteros
+            //int x;
+            //int g;
+            //// float para numeros decimales
+            //float y = 1.5f;
+            //// double para numeros decimales muy grandes
+            //double z = 1.00000000000000000000000000000000000000000000005d;
+            //// Char para letras, la comillasimple esta debajo del signo de cerrar interrogacion
+            //char letra = 'A';
+            //// string para frases
+            //string Frase = "Hola buenas tardes, ¿Me puedes dar un numero entero ahora?";
+            //// bool para verdadero falso
+            //bool Verdadero = true;
+            //string Saludo;
+            //// Console.WriteLine es para escribir en la consola
+            //Console.WriteLine("¿Me dices hola?");
+            //// Console.ReadLine le pide al usuario que escriba
+            //// Pedimos al usuario que salude al programa, por que la caballeria no esta muerta
+            //Saludo = Console.ReadLine();
+            //// Comprobamos si nos ha saludado o no
+            //if ((Saludo == "Hola") || (Saludo=="hola"))
+            //{
+            //    // Como nos ha saludado le pedimos un numero entero para sumar
+            //    Console.WriteLine(Frase);
+            //    // Esto lo de int y todo eso es para comprobar que el numero que te da el usuario es entero
+            //    // Comprobamos si el numero es entero o no
+            //    if (Int32.TryParse(Console.ReadLine(), out x))
+            //    {
+            //        // Como el numero es entero le pedimos el otro numero
+            //        Console.WriteLine("Ahora dame otro numero entero por favor");
+            //        // Comprobamos si el otro numero es entero o no
+            //        if (Int32.TryParse(Console.ReadLine(), out g))
+            //        {
+            //            // Sumamos los dos numeros enteros y se lo mostramos al usuario
+            //            Console.WriteLine("La suma es " + (x + g));
+            //            Console.WriteLine("La resta es " + (x - g));
+            //            Console.WriteLine("La multiplicacion es " + (x * g));
+            //            Console.WriteLine("La division es " + (x / g));
+            //            Console.WriteLine("Hasta luego");
+            //            Thread.Sleep(2000);
+            //        } 
+            //        else
+            //        {
+            //            // El numero no ha sido entero
+            //            Console.WriteLine("Eso no es un numero entero");
+            //            Thread.Sleep(2000);
+            //        }
+            //    }
+            //    else
+            //    // El numero no ha sido entero
+            //    {
+            //        Console.WriteLine("Eso no es un numero entero");
+            //        Thread.Sleep(2000);
+            //    }
+            //}
+            //else
+            //{
+            //    // El usuario no ha saludado al programa
+            //    Console.WriteLine("Eres un maleducado");
+            //    Thread.Sleep(2000);
+            //}
+            ////Ejerciocio 02
+            //Console.WriteLine("¿Que edad tienes?");
+            //int edad = int.Parse(Console.ReadLine());
+            //if (edad < 18)
+            //{
+            //    Console.WriteLine("Eres menor de edad");
+            //    Thread.Sleep(2000);
+            //} else if(edad >= 65)
+            //{
+            //    Console.WriteLine("Eres un jubilado");
+            //    Thread.Sleep(2000);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eres mayor de edad");
+            //    Thread.Sleep(2000);
+            //}
+            ////Ejercicio 03
+            //Console.WriteLine("¿Que nota has sacado?");
+            //float nota = float.Parse(Console.ReadLine());
+            //if (nota >= 0 && nota<=10)
+            //{
+            //    if (nota < 5)
+            //    {
+            //        Console.WriteLine("Suspenso");
+            //        Thread.Sleep(2000);
+            //    }
+            //    else if (nota>= 5 && nota<6)
+            //    {
+            //        Console.WriteLine("Aprovado");
+            //        Thread.Sleep(2000);
+            //    }
+            //    else if (nota>= 6 && nota<7)
+            //    {
+            //        Console.WriteLine("Bien");
+            //        Thread.Sleep(2000);
+            //    }
+            //    else if (nota>6 && nota < 9)
+            //    {
+            //        Console.WriteLine("Notable");
+            //        Thread.Sleep(2000);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Sobresaliente");
+            //        Thread.Sleep(2000);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No es una nota valida");
+            //    Thread.Sleep(2000);
+            //}4
+            //Ejercicio 04
+            
+            int nota =0;
+            while (nota != -1) {
+                Console.WriteLine("¿Que nota has sacado?");
+                nota = int.Parse(Console.ReadLine());
+                switch (nota)
+            {
+                    case -1:
+                        break;
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("suspenso");
+                        break;
+                case 5:
+                    Console.WriteLine("Aprovado");
+                    
+                    break;
+                case 6:
+                    Console.WriteLine("Bien");
+                    
+                    break;
+                case 7:
+                case 8:
+                    Console.WriteLine("Notable");
+                    
+                    break;
+                case 9:
+                case 10:
+                    Console.WriteLine("Sobresaliente");
+                    break;
+                default:
+                    Console.WriteLine("No es una nota valida");
+                    break;
+            }
+            }
+            Console.WriteLine("Saliendo");
+            Thread.Sleep(2000);
+        }
+    }
+}
