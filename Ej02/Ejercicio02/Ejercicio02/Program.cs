ad01bc68e0b64b1e0d3fe16bd377e829922a620d
Sergio BG <sergibelgo1@gmail.com>
2020-02-20 10:11:34 +0100

    Act

--- Ej02/Ejercicio02/Ejercicio02/Program.cs
+++ Ej02/Ejercicio02/Ejercicio02/Program.cs
@@ -9,24 +9,34 @@ namespace Ejercicio02
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Dame un numero");
-            int numero = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            for (int i = 0; i <= numero; i++)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine();
-            int o = 0;
-            do
-            {
-                Console.WriteLine(o);
-                o++;
-            } while (o<=numero);
-            Thread.Sleep(2000);
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Dame un numero");
+        //    int numero = int.Parse(Console.ReadLine());
+        //    Console.WriteLine();
+        //    int numbfin = 0;
+        //    for (int i = 0; i <= numero; i++)
+        //    {
+        //        numbfin = numbfin + i;
+        //    }
+        //    Console.WriteLine("El resultado de la suma de todos los numeros consecutivos es: " + numbfin);
+        //    Thread.Sleep(4000);
 
+        //}
+        //Ejemplo para crear otras funciones distintas a la Main
+        static void Main()
+        {
+            Console.WriteLine("Dame millas y las convertire en Km");
+            float Muser = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(MillastoKm(Muser) + "Km");
+            Thread.Sleep(2000);
+        }
+        //Para crear otras funciones que no sean la main-->
+        static float MillastoKm (float Km)
+        {
+            float millas = Km * 1.60934f;
+            return millas;
         }
     }
 }
