f9469792ca35042dae418cd9db26d734c64db620
Sergio BG <sergibelgo1@gmail.com>
2020-02-21 10:14:03 +0100

    Update Program.cs

--- Arrays/Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Arrays/Program.cs
@@ -41,16 +41,53 @@ namespace Arrays
             //numeros[0] = 0;
             //Console.WriteLine(numeros[0] + " y " + numeros02[0]);
             //Thread.Sleep(6000);
-            int x = 3;
-            int y = 4;
-            Console.WriteLine("Praa " + x + " " + y);
-            int comodin = 0;
-            Console.WriteLine();
-            comodin = y;
-            y = x;
-            x = comodin;
-            Console.WriteLine("Pum " + x + " " + y);
+            //int x = 3;
+            //int y = 4;
+            //Console.WriteLine("Praa " + x + " " + y);
+            //int comodin = 0;
+            //Console.WriteLine();
+            //comodin = y;
+            //y = x;
+            //x = comodin;
+            //Console.WriteLine("Pum " + x + " " + y);
+            //Thread.Sleep(5000);
+            //Piramide de asteriscos
+            ////Console.WriteLine("Introduce cuantas filas quieres");
+            ////int aux = 1;
+            ////int numero = int.Parse(Console.ReadLine());
+            ////for (int i = 0; i < numero; i++)
+            ////{
+            ////    for (int j = 0; j < aux; j++)
+            ////    {
+            ////        Console.Write("*");
+            ////    }
+            ////    Console.WriteLine();
+            ////    aux++;
+            ////}
+            ////Thread.Sleep(5000);
+            //_-_-_
+            //_xxx-
+            //xxxxx
+            //int numero = int.Parse(Console.ReadLine());
+            //int aux = 0;
+            //int aux02 = numero / 2;
+            //char[] letra = new char[numero];
+            //for (int i = 0; i < numero; i++)
+            //{
+            //    for (int j = 0; j < aux; j++)
+            //    {
+            //        if (numero%2==0)
+            //        {
+
+
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+            int[] numeros = { 1, 2, 3, 0, 1, 4 };
+            Console.Write(numeros);
             Thread.Sleep(5000);
+
         }
     }
 }
