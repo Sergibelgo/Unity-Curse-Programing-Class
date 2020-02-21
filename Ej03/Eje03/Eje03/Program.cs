13d6f56bb65d75e05836d8294acfabf22ca43fcb
Sergio BG <sergibelgo1@gmail.com>
2020-02-21 13:13:24 +0100

    Update Program.cs

--- Ej03/Eje03/Eje03/Program.cs
+++ Ej03/Eje03/Eje03/Program.cs
@@ -13,7 +13,15 @@ namespace Eje03
             //para las listas se escribe asi-->
             List<int> numbers = new List<int>();
             List<float> asd = new List<float>();
-            List<char> asd = new List<char>();
+            List<char> asddas = new List<char>();
+            //Añade al final de la cadena
+            asd.Add(1);
+            asd.Add(2);
+            asd.Add(30);
+            //Elemina el primer contenedor que contenga ese numero
+            asd.Remove(1);
+            //Elemina el contenedor con el indice que especifiques
+            asd.RemoveAt(1);
         }
     }
 }
